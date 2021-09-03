8a8c759f6ed8dc9dd85f055d1927e5decb12ffd3
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-03 17:23:17 +0530

    Added new component

--- expense_tracker/src/components/ExpenseItems.js
+++ expense_tracker/src/components/ExpenseItems.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItems() {
-  return <h1>Expense Items</h1>;
+  const expenseDate = new Date();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 15000;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs.{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItems;
