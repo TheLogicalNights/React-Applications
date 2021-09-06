973b5b4bc3e4993dbc95fd6f1b7f06cdd6e05f19
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-06 16:26:19 +0530

    Updated

--- expense_tracker/src/components/ExpenseItems.js
+++ expense_tracker/src/components/ExpenseItems.js
@@ -1,17 +1,9 @@
 import "./ExpenseItem.css";
-
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItems(props) {
-  const day = props.date.toLocaleString("en-US", { day: "numeric" });
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs.{props.amount}</div>
