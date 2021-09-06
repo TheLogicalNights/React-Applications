3dbf3289a9c86b2a0a57db73f275d4f124abf380
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-06 12:28:12 +0530

    Implemented props

--- expense_tracker/src/components/ExpenseItems.js
+++ expense_tracker/src/components/ExpenseItems.js
@@ -1,16 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItems() {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 15000;
+function ExpenseItems(props) {
+  const day = props.date.toLocaleString("en-US", { day: "numeric" });
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">Rs.{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs.{props.amount}</div>
       </div>
     </div>
   );
