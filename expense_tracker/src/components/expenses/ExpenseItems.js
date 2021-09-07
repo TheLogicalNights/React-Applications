197dfa5c316aed8e6c24824636fbbc066b7f51c1
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-07 18:49:22 +0530

    composed all components into one folder

---
+++ expense_tracker/src/components/expenses/ExpenseItems.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../ui/Card";
+
+function ExpenseItems(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated !");
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">Rs.{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItems;
---
+++ expense_tracker/src/components/expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from "../ui/Card";
+import ExpenseItems from "./ExpenseItems";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItems
+        title={props.expense[0].title}
+        amount={props.expense[0].amount}
+        date={props.expense[0].date}
+      />
+      <ExpenseItems
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      />
+      <ExpenseItems
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      />
+      <ExpenseItems
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
