6e5b4f9e9bc98256f4572e7ec76f46f040bf6ff4
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-08 18:25:30 +0530

    Rendered expense items dynamically using map function

--- expense_tracker/src/components/expenses/Expenses.js
+++ expense_tracker/src/components/expenses/Expenses.js
@@ -14,26 +14,14 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpenseFilter selected={changedYear} onYearChange={yearChangeHandler} />
-      <ExpenseItems
-        title={props.expense[0].title}
-        amount={props.expense[0].amount}
-        date={props.expense[0].date}
-      />
-      <ExpenseItems
-        title={props.expense[1].title}
-        amount={props.expense[1].amount}
-        date={props.expense[1].date}
-      />
-      <ExpenseItems
-        title={props.expense[2].title}
-        amount={props.expense[2].amount}
-        date={props.expense[2].date}
-      />
-      <ExpenseItems
-        title={props.expense[3].title}
-        amount={props.expense[3].amount}
-        date={props.expense[3].date}
-      />
+
+      {props.expense.map((expense) => (
+        <ExpenseItems
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
