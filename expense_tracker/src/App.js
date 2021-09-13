1eaab5e4e2d4d26302f448e0718f83bce2b66578
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-13 11:54:05 +0530

    Added id for each expense

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -4,10 +4,30 @@ import NewExpense from "./components/addExpense/newExpense";
 import { useState } from "react";
 
 const DUMMY_EXPENSES = [
-  { title: "Car insurance", amount: 1500, date: new Date() },
-  { title: "Books", amount: 1000, date: new Date() },
-  { title: "Bike Servicing", amount: 900, date: new Date() },
-  { title: "New Keyboard", amount: 1100, date: new Date() },
+  {
+    id: Math.floor(Math.random() * 10000000000000000 + 1),
+    title: "Car insurance",
+    amount: 1500,
+    date: new Date(),
+  },
+  {
+    id: Math.floor(Math.random() * 10000000000000000 + 1),
+    title: "Books",
+    amount: 1000,
+    date: new Date(),
+  },
+  {
+    id: Math.floor(Math.random() * 10000000000000000 + 1),
+    title: "Bike Servicing",
+    amount: 900,
+    date: new Date(),
+  },
+  {
+    id: Math.floor(Math.random() * 10000000000000000 + 1),
+    title: "New Keyboard",
+    amount: 1100,
+    date: new Date(),
+  },
 ];
 
 function App() {
