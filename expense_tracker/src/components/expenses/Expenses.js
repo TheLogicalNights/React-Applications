e5381b4b1aa8b2ac063bd9232ebbe5a18ac36bdc
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-13 18:43:18 +0530

    Rendered individual component

--- expense_tracker/src/components/expenses/Expenses.js
+++ expense_tracker/src/components/expenses/Expenses.js
@@ -1,8 +1,8 @@
 import Card from "../ui/Card";
-import ExpenseItems from "./ExpenseItems";
 import "./Expenses.css";
 import ExpenseFilter from "../expenseFilter/ExpenseFilter";
 import { useState } from "react";
+import ExpenseList from "./ExpenseList";
 
 function Expenses(props) {
   const [changedYear, setChangedYear] = useState("2021");
@@ -15,51 +15,10 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === changedYear;
   });
 
-  let expenseContent = <p>No Expenses Found</p>;
-
-  if (filteredArray.length > 0) {
-    expenseContent = filteredArray.map((expense) => (
-      <ExpenseItems
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpenseFilter selected={changedYear} onYearChange={yearChangeHandler} />
-
-      {
-        /* {filteredArray.length === 0 ? (
-        <p>No Expenses Found</p>
-      ) : (
-        filteredArray.map((expense) => (
-          <ExpenseItems
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )} */
-        ////////////////////////// Second Approch ///////////////////////////////////////
-        //   filteredArray.length === 0 && <p>No Expenses Found</p>
-        // }
-        // {filteredArray.length > 0 &&
-        //   filteredArray.map((expense) => (
-        //     <ExpenseItems
-        //       key={expense.id}
-        //       title={expense.title}
-        //       amount={expense.amount}
-        //       date={expense.date}
-        //     />
-        //   ))}
-        ///////////////////////////// Third approch ////////////////////////////////////////
-        expenseContent
-      }
+      <ExpenseList array={filteredArray} />
     </Card>
   );
 }
