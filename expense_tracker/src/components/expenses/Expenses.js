6314fa037f8fa13a68f94d608d92b6e017d6f5fd
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-13 15:38:13 +0530

    Added conditional rendering

--- expense_tracker/src/components/expenses/Expenses.js
+++ expense_tracker/src/components/expenses/Expenses.js
@@ -15,18 +15,51 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === changedYear;
   });
 
+  let expenseContent = <p>No Expenses Found</p>;
+
+  if (filteredArray.length > 0) {
+    expenseContent = filteredArray.map((expense) => (
+      <ExpenseItems
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter selected={changedYear} onYearChange={yearChangeHandler} />
 
-      {filteredArray.map((expense) => (
-        <ExpenseItems
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {
+        /* {filteredArray.length === 0 ? (
+        <p>No Expenses Found</p>
+      ) : (
+        filteredArray.map((expense) => (
+          <ExpenseItems
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )} */
+        ////////////////////////// Second Approch ///////////////////////////////////////
+        //   filteredArray.length === 0 && <p>No Expenses Found</p>
+        // }
+        // {filteredArray.length > 0 &&
+        //   filteredArray.map((expense) => (
+        //     <ExpenseItems
+        //       key={expense.id}
+        //       title={expense.title}
+        //       amount={expense.amount}
+        //       date={expense.date}
+        //     />
+        //   ))}
+        ///////////////////////////// Third approch ////////////////////////////////////////
+        expenseContent
+      }
     </Card>
   );
 }
